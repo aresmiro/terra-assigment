8080772e0563f178edb24d28d7b69c4793986a8e
aresmiro <ares@rashediconsulting.de>
2024-11-04 19:13:44 +0100

    fixed styles and broken menu

--- src/App.jsx
+++ src/App.jsx
@@ -105,11 +105,11 @@ const App = () => {
         <img className="app__logo" src={data.navbar.logo} alt="Logo" />
         <button className="app__hamburger" onClick={toggleMenu}>
           {!isMenuOpen ? (
-            <div aria-label="Toggle menu">
+            <>
               <span className="app__bar"></span>
               <span className="app__bar"></span>
               <span className="app__bar"></span>
-            </div>
+            </>
           ) : (
             <svg className="app__close-icon" aria-label="Close menu" width="32" height="32" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
               <path d="M18 6L6 18M6 6L18 18" strokeWidth="2" strokeLinecap="round" />
